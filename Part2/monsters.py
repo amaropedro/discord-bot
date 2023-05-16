0ffee7d16968db1bed205913414b556123b87c69
Amaro <tpedro.amaro@gmail.com>
2023-05-16 17:49:22 -0300

    creates monster subclasses

---
+++ Part2/monsters.py
@@ -0,0 +1,22 @@
+"""
+Pedro Amaro
+"""
+
+class Monster():
+    def __init__(self, name, level, max_health):
+        self.name = name
+        self.level = level
+        self.health = max_health
+        self.max_health = max_health
+
+class Rat(Monster):
+    def __init__(self):
+        super().__init__("Rat", 1, 20)
+
+class Boar(Monster):
+    def __init__(self):
+        super().__init__("Boar", 2, 40)
+
+class Goblin(Monster):
+    def __init__(self):
+        super().__init__("Goblin", 3, 60)
